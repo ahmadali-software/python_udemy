0fd862821c3e3581c1870fef9cf1238700def9ce
ahmad <ahmadaliasdf15@gmail.com>
2026-09-10 13:43:21 -0500

    created a practice drwing app with turtle

--- day19/main.py
+++ day19/main.py
@@ -3,7 +3,34 @@ from turtle import *
 timmy = Turtle()
 screen = Screen()
 
-screen.listen()
-# non
+def move_forwards():
+    timmy.forward(10)
 
-# print("trying some new stuff")
+
+def move_backwards():
+    
+    timmy.backward(10)
+
+def move_right():
+    timmy.setheading(timmy.heading() + 5) 
+    # timmy.forward(10)
+
+def move_left():
+    timmy.setheading(timmy.heading() - 5) 
+    # timmy.forward(10)
+
+def clear():
+    timmy.clear()
+    timmy.penup()
+    timmy.goto(0,0)
+    timmy.pendown()
+
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="d", fun=move_right)
+screen.onkey(key="a", fun=move_left)
+screen.onkey(key="c", fun=clear)
+
+screen.exitonclick()
